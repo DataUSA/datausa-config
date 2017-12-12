19195bff84024447ff881418bd0f785c15c2a83c
jspeis <jspeis@gmail.com>
2017-12-12 08:38:02 -0500

    adds table for financial aid metrics broken out by income level

--- ipeds/plugins/sfa.py
+++ ipeds/plugins/sfa.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+
+
 def transform_living(df, **kwargs):
     if "pk" not in kwargs or not kwargs["pk"]:
         raise Exception("Please specify a valid primary key")
@@ -12,4 +16,27 @@ def transform_living(df, **kwargs):
     df.loc[df.variable == "num_living_unknown", "living_arrangement"] = "4"
 
     del df["variable"]
+    return df
+
+
+def transform_income_range(df, **kwargs):
+    if "pk" not in kwargs or not kwargs["pk"]:
+        raise Exception("Please specify a valid primary key")
+    pk = kwargs["pk"]
+
+    df = df.melt(id_vars=pk)  # value_name="variable")
+
+    df.variable = df.variable.str.strip()
+    df.loc[df.variable.str.endswith("level_1"), "income_range"] = "1"
+    df.loc[df.variable.str.endswith("level_2"), "income_range"] = "2"
+    df.loc[df.variable.str.endswith("level_3"), "income_range"] = "3"
+    df.loc[df.variable.str.endswith("level_4"), "income_range"] = "4"
+    df.loc[df.variable.str.endswith("level_5"), "income_range"] = "5"
+    df.variable = df.variable.str.slice(0, -8)
+    my_list = df.variable.unique()
+    df = pd.pivot_table(df, values="value", index=pk + ["income_range"], columns=["variable"], aggfunc=np.sum)
+    df = df.dropna(axis=0, how='all', subset=my_list)  # drop rows where are of the elements are nan
+
+    df = df.reset_index()
+
     return df
